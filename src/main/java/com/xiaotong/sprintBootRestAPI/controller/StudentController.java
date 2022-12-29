016884f89c82fb363daf1fbcd0c173c0083308a4
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-12-29 19:33:09 +1100

    return list as jason

--- src/main/java/com/xiaotong/sprintBootRestAPI/controller/StudentController.java
+++ src/main/java/com/xiaotong/sprintBootRestAPI/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.xiaotong.sprintBootRestAPI.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
     @GetMapping("student")
@@ -12,5 +15,15 @@ public class StudentController {
                 1, "Xiaotong","Wang"
         );
         return student;
+    }
+
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Xiaotong", "Wang"));
+        students.add(new Student(2, "Yuchen", "Zheng"));
+        students.add(new Student(3, "Dawang", "Zheng"));
+        students.add(new Student(4, "Balala", "Nengliang"));
+        return students;
     }
 }
