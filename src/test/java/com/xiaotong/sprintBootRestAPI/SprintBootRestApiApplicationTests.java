4945a84a8f291cc647de00e5e7bd1291f8ce2f50
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-12-29 18:49:08 +1100

    starter

---
+++ src/test/java/com/xiaotong/sprintBootRestAPI/SprintBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaotong.sprintBootRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
